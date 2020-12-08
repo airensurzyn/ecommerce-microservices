69dbc55f0d16d6d377cb4937ce10f27164e9a663
Airen <nyzrus@gmail.com>
2020-12-07 20:47:37 -0800

    Add ingress service and auth routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,16 @@
 import express from 'express';
+import { json } from 'body-parser';
+import { currentUserRouter } from './routes/current';
+import { signupRouter } from './routes/signup';
 
 const app = express();
+
+
 app.set('trust proxy', true);
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signupRouter);
 
 app.all('*', async (req, res) => {
     res.send('Good');
